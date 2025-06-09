efd1854cfcdf07a9871ba1a6485e1f7adc351665
Sam-Néri Bélizaire <samneribelizaire@icloud.com>
2025-06-09 16:35:45 -0400

    Implement JumboToRollsCalculator with methods for jumbo and snapoff roll calculations

--- JumboToRolls.API/Services/JumboToRollsCalculator.cs
+++ JumboToRolls.API/Services/JumboToRollsCalculator.cs
@@ -1,6 +1,21 @@
+using JumboToRolls.API.DTOs;
+
 namespace JumboToRolls.API.Services;
 
-public class JumboToRollsCalculator
+public class JumboToRollsCalculator : IJumboToRollsCalculator
 {
+    
+    
+    public RollCalculationResult CalculateFromJumbo(int jumboPaperRadius, string coreType )
+    {
+        // logic here
+        return new RollCalculationResult();
+    }
+
+    public RollCalculationResult CalculateFromSnapoff(int snapoffRadius, string coreType, List<string>? availableOrders)
+    {
+        // logic here
+        return new RollCalculationResult();
+    }
     
 }
